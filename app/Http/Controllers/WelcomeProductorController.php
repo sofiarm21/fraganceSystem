a765589ad29d7838103a29029820751df2813cc4
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-07-18 15:16:48 -0400

    database set up

--- app/Http/Controllers/WelcomeProductorController.php
+++ app/Http/Controllers/WelcomeProductorController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Productor;
 
 class WelcomeProductorController extends Controller
 {
     public function index(){
 
+        $productores = Productor::all();
 
+        foreach ($productores as $productor) {
+            echo $productor;
+        }die;
 
         return view('welcomeProductor', [
-            
+
         ]);
     }
 }
