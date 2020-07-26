c0ad50b068391d4c9434c88f1d9d3ec4c95f88ee
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-07-26 01:40:18 -0400

    fix modulo evaluacion contrato detail

--- app/Http/Controllers/Evaluación/GeneracionContratoController.php
+++ app/Http/Controllers/Evaluación/GeneracionContratoController.php
@@ -206,18 +206,18 @@ class GeneracionContratoController extends Controller
                 );
         }
         if ($request->ingrediente_codigo != null){
-        foreach($request->ingrediente_codigo as $ingrediente_codigo){
+            foreach($request->ingrediente_codigo as $ingrediente_codigo){
+                echo "seleccionado $$ingrediente_codigo";
+                    DB::table('sms_det_contrato')->insert(
+                        [
+                            'cod_contrato' => $contrato->codigo,
+                            'id_productor' => $id_productor,
+                            'id_proveedor' => $id_proveedor,
+                            'cod_componente_ing' => $ingrediente_codigo
+                        ]
+                    );
 
-                DB::table('sms_det_contrato')->insert(
-                    [
-                        'cod_contrato' => $contrato->codigo,
-                        'id_productor' => $id_productor,
-                        'id_proveedor' => $id_proveedor,
-                        'cod_componente_ing' => $ingrediente_codigo
-                    ]
-                );
-
-        }
+            }
         }
 
         foreach($request->condiciones_pago as $cod_condicion_pago){
